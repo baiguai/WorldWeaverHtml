da36f230befd233c6afd41b557a249dd1cac9ac8
baiguai <baiguai.junk@gmail.com>
2026-04-19 03:07:23 -0700

    New Clue
    * Added a clue to the port.
    * Updated the diagram.
    * Fixes to the dev notes feature.

--- svr_help.js
+++ svr_help.js
@@ -37,7 +37,7 @@ Then simply open your notes .html file - to save use the key binding: n
 // Path to the HTML file you want to save
 const FILE_PATH = "./help.html";
 const NODE_IP = "localhost";  // or your LAN IP
-const PORT = 3002;
+const PORT = 3003;
 
 // Backup behavior
 const BACKUP_DIR = "./backups";   // where backups go
