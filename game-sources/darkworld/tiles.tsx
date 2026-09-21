85da551a071aeb02c21a013ba553dcf753af979d
baiguai <baiguai.junk@gmail.com>
2026-09-21 00:44:23 -0700

    Kitchen work - and refactoring room keys.

---
+++ game-sources/darkworld/tiles.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="main" tilewidth="128" tileheight="128" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="compass.png"/>
+ </tile>
+</tileset>
